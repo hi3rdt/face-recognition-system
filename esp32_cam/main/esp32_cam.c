711a4f5713b5e2471899f2e2b58c61f71402df17
hi3rdt <tuduchiep123@gmail.com>
2026-05-10 22:56:48 +0700

    feat(cam): add main execution flow for camera node

--- esp32_cam/main/esp32_cam.c
+++ esp32_cam/main/esp32_cam.c
@@ -1,6 +1,29 @@
-#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "nvs_flash.h"
+#include "camera_driver.h"
+#include "wifi_client.h"
+#include "websocket_client.h"
+#include "sensor_hcsr04.h"
 
-void app_main(void)
-{
+void app_main(void) {
+    esp_err_t ret = nvs_flash_init();
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+        nvs_flash_erase();
+        nvs_flash_init();
+    }
 
+   
+    camera_driver_init();
+
+    
+    wifi_client_init("ESP32CAM_to_ESP32", "myesp32server");
+
+    
+    websocket_client_init("ws://192.168.1.1:8888");
+
+    
+    sensor_hcsr04_init();
+
+    vTaskDelete(NULL);
 }
